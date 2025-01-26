571535cc52c6fe8f4e292849f2440b6e13e5a00e
ahmadreduan <reduanahmad363737@gmail.com>
2025-01-26 23:24:52 +0600

    User Login Successfully

--- app/src/main/java/com/ahmadreduan/amitumi/SignInActivity.java
+++ app/src/main/java/com/ahmadreduan/amitumi/SignInActivity.java
@@ -1,44 +1,42 @@
 package com.ahmadreduan.amitumi;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.ahmadreduan.amitumi.Models.Users;
-import com.ahmadreduan.amitumi.databinding.ActivitySignUpBinding;
+import com.ahmadreduan.amitumi.databinding.ActivitySignInBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUpActivity extends AppCompatActivity {
 
-    ActivitySignUpBinding binding;
-    private FirebaseAuth auth;
-    FirebaseDatabase database;
+public class SignInActivity extends AppCompatActivity {
 
+
+    ActivitySignInBinding binding;
     ProgressDialog progressDialog;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
-       // setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -46,53 +44,33 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
 
-
-
-
-
         auth = FirebaseAuth.getInstance();
 
-        database = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(SignInActivity.this);
+        progressDialog.setTitle("Login Account");
+        progressDialog.setMessage("Login to your account");
 
-        progressDialog = new ProgressDialog(SignUpActivity.this);
-        progressDialog.setTitle("Creating Account");
-        progressDialog.setMessage("We're creating your account");
 
-        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 progressDialog.show();
-                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString())
+                auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString())
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-
                                 progressDialog.dismiss();
-
-
                                 if(task.isSuccessful()){
-                                    Users users = new Users(binding.etUserName.getText().toString(), binding.etEmail.getText().toString(),
-                                            binding.etPassword.getText().toString());
-
-                                    String id = task.getResult().getUser().getUid();
-                                    database.getReference().child("Users").child(id).setValue(users);
-
-
-
-                                    Toast.makeText(SignUpActivity.this,"User Created Successfully", Toast.LENGTH_SHORT).show();
-                                }else {
-                                    Toast.makeText(SignUpActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                    startActivity(intent);
                                 }
-
+                                else {
+                                    Toast.makeText(SignInActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
                             }
                         });
-
             }
         });
-
-
-
-
 
     }
 }

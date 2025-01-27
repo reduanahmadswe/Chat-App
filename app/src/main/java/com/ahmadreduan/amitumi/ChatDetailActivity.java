7223b1b03ee4048222a5c823746f5dcf13cfaf18
ahmadreduan <reduanahmad363737@gmail.com>
2025-01-27 20:01:27 +0600

    done chat activity_chat_detail.xml design

---
+++ app/src/main/java/com/ahmadreduan/amitumi/ChatDetailActivity.java
@@ -0,0 +1,33 @@
+package com.ahmadreduan.amitumi;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.ahmadreduan.amitumi.databinding.ActivityChatDetailBinding;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+    }
+}

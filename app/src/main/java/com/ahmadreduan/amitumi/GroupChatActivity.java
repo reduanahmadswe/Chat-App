981335c3be1bd1f7477e18587213d19e0ac78b84
ahmadreduan <reduanahmad363737@gmail.com>
2025-02-16 19:31:24 +0600

    group chat BackEnd

--- app/src/main/java/com/ahmadreduan/amitumi/GroupChatActivity.java
+++ app/src/main/java/com/ahmadreduan/amitumi/GroupChatActivity.java
@@ -1,18 +1,34 @@
 package com.ahmadreduan.amitumi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.ahmadreduan.amitumi.Adapters.ChatAdapter;
+import com.ahmadreduan.amitumi.Models.MessageModel;
+import com.ahmadreduan.amitumi.databinding.ActivityGroupChatBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class GroupChatActivity extends AppCompatActivity {
+
+    ActivityGroupChatBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityGroupChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_group_chat);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +36,31 @@ public class GroupChatActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GroupChatActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final String SenderID = FirebaseAuth.getInstance().getUid();
+        binding.userName.setText("Friends Group");
+
+        final ChatAdapter adapter = new ChatAdapter(messageModels, this);
+        binding.chatRecyclarView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclarView.setLayoutManager(layoutManager);
+
+        bin
+
+
     }
+
 }

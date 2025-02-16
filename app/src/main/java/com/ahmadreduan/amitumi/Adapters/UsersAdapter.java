4f0a6220fe663bb07013d66369a6c53ee647f5ab
ahmadreduan <reduanahmad363737@gmail.com>
2025-02-16 21:27:12 +0600

    Show last message on users list

--- app/src/main/java/com/ahmadreduan/amitumi/Adapters/UsersAdapter.java
+++ app/src/main/java/com/ahmadreduan/amitumi/Adapters/UsersAdapter.java
@@ -1,22 +1,30 @@
 package com.ahmadreduan.amitumi.Adapters;
+
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ahmadreduan.amitumi.ChatDetailActivity;
 import com.ahmadreduan.amitumi.Models.Users;
 import com.ahmadreduan.amitumi.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import android.content.Context;
+
 import java.util.ArrayList;
 
-public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder>{
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
 
     ArrayList<Users> list;
 
@@ -30,7 +38,7 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder>{
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user, parent, false);
 
         return new ViewHolder(view);
     }
@@ -42,20 +50,39 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder>{
         Picasso.get().load(users.getProfilepic()).placeholder(R.drawable.user).into(holder.image);
         holder.userName.setText(users.getUserName());
 
-       //holder.lastMessage.setText(users.getLastMessage());
+        FirebaseDatabase.getInstance().getReference().child("chats")
+                .child(FirebaseAuth.getInstance().getUid() + users.getUserId())
+                .orderByChild("timestamp")
+                .limitToLast(1)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChildren()) {
+                            for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                                holder.lastMessage.setText(snapshot1.child("message").getValue(String.class).toString());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        //holder.lastMessage.setText(users.getLastMessage());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(context , ChatDetailActivity.class);
-                intent.putExtra("userID" , users.getUserId());
-                intent.putExtra("profilePic" , users.getProfilepic());
-                intent.putExtra("userName" , users.getUserName());
+                Intent intent = new Intent(context, ChatDetailActivity.class);
+                intent.putExtra("userID", users.getUserId());
+                intent.putExtra("profilePic", users.getProfilepic());
+                intent.putExtra("userName", users.getUserName());
                 context.startActivity(intent);
 
             }
         });
-
 
 
     }
@@ -68,7 +95,7 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView image;
-        TextView userName,lastMessage;
+        TextView userName, lastMessage;
 
 
         public ViewHolder(@NonNull View itemView) {

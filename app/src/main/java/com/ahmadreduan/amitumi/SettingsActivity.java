b6840440739c9a30ca837f9abd2fe843e01dfa77
ahmadreduan <reduanahmad363737@gmail.com>
2025-02-16 22:50:38 +0600

    Setting Activity BackEnd

--- app/src/main/java/com/ahmadreduan/amitumi/SettingsActivity.java
+++ app/src/main/java/com/ahmadreduan/amitumi/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.ahmadreduan.amitumi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ahmadreduan.amitumi.databinding.ActivitySettingsBinding;
+import com.ahmadreduan.amitumi.databinding.ActivitySignInBinding;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_settings);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
